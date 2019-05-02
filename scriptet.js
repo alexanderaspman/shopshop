a651700f845d71f1c98459716dab71e11f65511d
Alexander Aspman <aspmanalexander@gmail.com>
2019-05-02 10:23:59 +0200

    Add files via upload

--- scriptet.js
+++ scriptet.js
@@ -26,39 +26,8 @@ $(function () {
           let a = JSON.parse(window.localStorage.getItem(key));
         
         if (key > 0) {
-          $(".cart_items_list").append(
-            `
-       <div class="product_image"><img class="imgimg"  alt=""><img src="${a["3"]}"></div>
-							<div class="product_content">
-								<div class="product_info d-flex flex-row align-items-start justify-content-start">
-									<div>
-										<div>
-											<div class="product_name">${a["0"]}, ${a["1"]}<a href="cart.html"></a>	<div></div></div>
-											
-											
-										</div>
-									</div>
-									<div class="ml-auto text-right">
-										<div class="rating_r rating_r_4 home_item_rating"><i></i><i></i><i></i><i></i><i></i></div>
-										<div class="product_price text-right"><p class="price">${a["2"]}</p></div>
-									</div>
-								</div>
-								<div class="product_buttons">
-									<div class="text-right d-flex flex-row align-items-start justify-content-start">
-										<div class="product_button product_fav text-center d-flex flex-column align-items-center justify-content-center">
-											<div><div><img src="images/heart.png" class="svg" alt=""><div>+</div></div></div>
-										</div>
-										
-										<div class="product_button product_cart text-center d-flex flex-column align-items-center justify-content-center">
-											<div><div><a href="cart.html"><img src="images/cart.png" class="svg" alt=""></a><div>+</div></div></div>
-										</div>
-									
-								
-									</div>
-								</div>
-							</div>
+       
             
-            `)
           
         }
       }
